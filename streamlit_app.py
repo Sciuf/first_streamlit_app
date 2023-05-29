6835ebdf69d2e696c07712a3746f7569851056cb
Sciuf <134630343+Sciuf@users.noreply.github.com>
2023-05-29 11:53:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,9 @@ if streamlit.button('Get fruitLoadList'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 #streamlit.stop()
-def Snow_insert(add_my_fruit):
+def Snow_insert(add_my_fruit): #"jackfruit", "papaya", "guava" and "kiwi"
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('" + add_my_fruit + "')")
         return 'The user entered ' + add_my_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
